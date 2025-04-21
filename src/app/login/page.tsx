1413983fdeab5a1d1d87eb8dfb49f882cb12c3b8
Thairone-J <thairone.dev@gmail.com>
2025-04-21 16:20:04 -0300

    feat: add login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,17 @@
+'use client';
+
+import AuthForm from '@/components/AuthForm/AuthForm';
+import styles from './page.module.scss';
+
 export default function LoginPage() {
-  return <h1>Login page</h1>;
+  const handleLogin = () => {
+    alert();
+  };
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.formWrapper}>
+        <AuthForm mode="login" onSubmit={handleLogin} />
+      </div>
+    </div>
+  );
 }
