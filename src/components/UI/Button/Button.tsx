ae65220f0f314c34a5d1436b7c30a5a1c1ba348f
Thairone-J <thairone.dev@gmail.com>
2025-04-21 16:11:38 -0300

    fix: add missing button props

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -2,19 +2,22 @@ import styles from './Button.module.scss';
 
 type ButtonProps = {
   children: React.ReactNode;
-  variant?: 'primary' | 'secondary';
+  variant?: 'primary' | 'secondary' | 'outline';
   fontSize?: 'small' | 'medium' | 'large';
+  type?: 'button' | 'submit' | 'reset';
   onClick?: () => void;
 };
 
 export default function Button({
   children,
+  type,
   variant = 'primary',
   fontSize = 'small',
   onClick,
 }: ButtonProps) {
   return (
     <button
+      type={type}
       className={`${styles.button} ${styles[variant]} ${styles[`font-${fontSize}`]}`}
       onClick={onClick}
     >
