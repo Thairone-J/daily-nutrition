598caaca6c47a54dbb152470ded29f692c2291d7
Thairone-J <thairone.dev@gmail.com>
2025-04-24 01:16:45 -0300

    feat: add side panel header

---
+++ src/components/SidePanel/Header.tsx
@@ -0,0 +1,24 @@
+import styles from './Header.module.scss';
+
+type HeaderProps = {
+  calorieGoal?: number;
+  consumedCalories?: number;
+};
+
+export default function Header({ calorieGoal, consumedCalories }: HeaderProps) {
+  consumedCalories = 0;
+  calorieGoal = 2500;
+
+  return (
+    <div className={styles.wrapper}>
+      <img src="pp.jpg" alt="" />
+      <div className={styles.kcalGoal}>
+        <h2>Metas nutricionais</h2>
+        <p>
+          <span className={styles.consumedCalories}>{consumedCalories}</span> de {calorieGoal} calorias
+          🔥
+        </p>
+      </div>
+    </div>
+  );
+}
