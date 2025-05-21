2464dfc214bb7d31a809de003e574719946cf09e
Thairone-J <thairone.dev@gmail.com>
2025-05-21 02:30:18 -0300

    refactor: adjust selected food style

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,11 +3,7 @@
 import styles from './page.module.scss';
 import SidePanel from '@/components/SidePanel/SidePanel';
 import SearchBar from '@/components/SearchBar/SearchBar';
-import { useState } from 'react';
-import { Food } from '@/context/MealsContext';
 import { useFoods } from '@/context/FoodsContext';
-import Icon from '@/components/UI/Icon/Icon';
-import Button from '@/components/UI/Button/Button';
 
 export default function Dashboard() {
   const { currentFood } = useFoods();
@@ -23,38 +19,30 @@ export default function Dashboard() {
           <SearchBar />
         </div>
         <div className={styles.currentFoodDisplayArea}>
-          <div className={`${currentFood ? styles.display : styles.hide}`}>
-            {currentFood ? (
-              <>
-                <div className={styles.addFoodToMealWrapper}></div>
-              </>
-            ) : (
-              <></>
-            )}
-            {currentFood ? (
+          {currentFood ? (
+            <>
               <div className={styles.foodCard}>
                 <h3 className={styles.foodName}>{currentFood.name}</h3>
-
-                <div className={styles.nutrientTagsContainer}>
-                  <span>️‍🔥 {currentFood.kcal}kcal</span>
-                  <span> Carboidrato: {currentFood.carbohydrates}g</span>
-                  <span>Proteína: {currentFood.protein}g</span>
-                  <span>Gordura: {currentFood.lipids}g</span>
-                </div>
-
-                <div className={styles.controlsContainer}>
-                  <div className={styles.quantityDisplay}>
-                    <span className={styles.quantityText}>{currentFood.quantity}g</span>
-                    <Icon name="edit" size={20} />
+                <div className={styles.infoRow}>
+                  <div className={styles.nutrientTagsContainer}>
+                    <span>️‍🔥 {currentFood.kcal}kcal</span>
+                    <span>Proteína: {currentFood.protein}g</span>
+                    <span> Carboidrato: {currentFood.carbohydrates}g</span>
+                    <span>Gordura: {currentFood.lipids}g</span>
                   </div>
-
-                  <Button fontSize="medium"> Adicionar a uma refeição</Button>
+                  <div className={styles.controls}>
+                    <input type="number" placeholder={currentFood.quantity + 'g'} />
+                    <button>Adicionar a uma refeição</button>
+                  </div>
                 </div>
+                {/* Implementar add food to meal    
+                  Date pick | Meal Select | Confirm button
+                */}
               </div>
-            ) : (
-              <></>
-            )}
-          </div>
+            </>
+          ) : (
+            <></>
+          )}
         </div>
       </div>
     </div>
