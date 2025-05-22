d1a4755ecb7bfb0e45ca62c01667d4524aa1e32a
Thairone-J <thairone.dev@gmail.com>
2025-05-22 03:10:04 -0300

    feat: add closing food card animation

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,9 +4,16 @@ import styles from './page.module.scss';
 import SidePanel from '@/components/SidePanel/SidePanel';
 import SearchBar from '@/components/SearchBar/SearchBar';
 import { useFoods } from '@/context/FoodsContext';
+import { useState } from 'react';
 
 export default function Dashboard() {
-  const { currentFood } = useFoods();
+  const { currentFood, setCurrentFood } = useFoods();
+  const [isClosing, setIsClosing] = useState(false);
+
+  const handleAnimationEnd = () => {
+    setCurrentFood(undefined);
+    setIsClosing(false);
+  };
 
   return (
     <div className={styles.wrapper}>
@@ -21,20 +28,34 @@ export default function Dashboard() {
         <div className={styles.currentFoodDisplayArea}>
           {currentFood ? (
             <>
-              <div className={styles.foodCard}>
-                <h3 className={styles.foodName}>{currentFood.name}</h3>
-                <div className={styles.infoRow}>
-                  <div className={styles.nutrientTagsContainer}>
-                    <span>️‍🔥 {currentFood.kcal}kcal</span>
-                    <span>Proteína: {currentFood.protein}g</span>
-                    <span> Carboidrato: {currentFood.carbohydrates}g</span>
-                    <span>Gordura: {currentFood.lipids}g</span>
-                  </div>
-                  <div className={styles.controls}>
-                    <input type="number" placeholder={currentFood.quantity + 'g'} />
-                    <button>Adicionar a uma refeição</button>
-                  </div>
+              <div
+                className={`${styles.foodCard} ${isClosing ? styles.isClosingAnimation : ''} `}
+                onAnimationEnd={isClosing ? handleAnimationEnd : undefined}
+              >
+                <div className={styles.header}>
+                  <span
+                    onClick={() => {
+                      setIsClosing(!isClosing);
+                    }}
+                  >
+                    X
+                  </span>
+                  <h3 className={styles.foodName}>{currentFood.name.toUpperCase()}</h3>
                 </div>
+
+                <div className={styles.nutrientTagsContainer}>
+                  <span> ️‍🔥 {currentFood.kcal}kcal</span>
+                  <span>Proteína: {currentFood.protein}g</span>
+                  <span> Carboidrato: {currentFood.carbohydrates}g</span>
+                  <span className={styles.lipid}>Gordura: {currentFood.lipids}g</span>
+                </div>
+
+                <div className={styles.controls}>
+                  {' '}
+                  <input type="number" placeholder={currentFood.quantity + 'g'} />
+                  <button onClick={() => {}}>Adicionar a uma refeição</button>
+                </div>
+
                 {/* Implementar add food to meal    
                   Date pick | Meal Select | Confirm button
                 */}
--- src/components/SidePanel/SidePanel.tsx
+++ src/components/SidePanel/SidePanel.tsx
@@ -29,7 +29,7 @@ export default function SidePanel() {
             addMeal(newMeal);
           }}
         >
-          Adicionar refeição
+          Criar refeição
         </Button>
       </div>
 
