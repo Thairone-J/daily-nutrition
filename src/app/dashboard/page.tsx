72efeed874fdd0eaa0e04c94ee20246e207c470f
Thairone-J <thairone.dev@gmail.com>
2025-05-23 03:45:04 -0300

    feat: add food to meal selection and date input

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,14 +5,26 @@ import SidePanel from '@/components/SidePanel/SidePanel';
 import SearchBar from '@/components/SearchBar/SearchBar';
 import { useFoods } from '@/context/FoodsContext';
 import { useState } from 'react';
+import { useMeals } from '@/context/MealsContext';
 
 export default function Dashboard() {
   const { currentFood, setCurrentFood } = useFoods();
   const [isClosing, setIsClosing] = useState(false);
+  const [isAddingToMeal, setIsAddingToMeal] = useState(false);
+  const { meals } = useMeals();
+
+  const date = new Date();
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+  const formattedMonth = String(month).padStart(2, '0');
+  const formattedDay = String(day).padStart(2, '0');
+  const formattedTodayDate = `${year}-${formattedMonth}-${formattedDay}`;
 
   const handleAnimationEnd = () => {
     setCurrentFood(undefined);
     setIsClosing(false);
+    setIsAddingToMeal(false);
   };
 
   return (
@@ -51,14 +63,33 @@ export default function Dashboard() {
                 </div>
 
                 <div className={styles.controls}>
-                  {' '}
                   <input type="number" placeholder={currentFood.quantity + 'g'} />
-                  <button onClick={() => {}}>Adicionar a uma refeição</button>
+                  {isAddingToMeal ? (
+                    <>
+                      <div className={styles.dateMealInputWrapper}>
+                        <input
+                          type="date"
+                          defaultValue={formattedTodayDate}
+                          className={styles.dateInput}
+                        />
+                        <select name="selectMeal" id="selectMeal">
+                          {meals.map((meal) => (
+                            <option key={meal.id}>{meal.title}</option>
+                          ))}
+                        </select>
+                      </div>
+                    </>
+                  ) : (
+                    ''
+                  )}
+                  <button
+                    onClick={() => {
+                      setIsAddingToMeal(!isAddingToMeal);
+                    }}
+                  >
+                    Adicionar a uma refeição
+                  </button>
                 </div>
-
-                {/* Implementar add food to meal    
-                  Date pick | Meal Select | Confirm button
-                */}
               </div>
             </>
           ) : (
