6ede99b2c82bb263e529444c68be97031baa62ca
Thairone-J <thairone.dev@gmail.com>
2025-05-28 01:49:53 -0300

    feat: add meal creation date

--- src/components/SidePanel/SidePanel.tsx
+++ src/components/SidePanel/SidePanel.tsx
@@ -3,13 +3,17 @@
 import styles from './SidePanel.module.scss';
 import Header from './GoalsStats/Header';
 import GoalsStats from './GoalsStats/GoalsStats';
-import MealsList from '../MealsList/MealsList';
+import MealsList from '../MealCardList/MealCardList';
 import Button from '../UI/Button/Button';
 import { useMeals } from '@/context/MealsContext';
-import { title } from 'process';
 
 export default function SidePanel() {
   const { meals, addMeal } = useMeals();
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const today = `${year}-${month}-${day}`;
 
   return (
     <div className={styles.wrapper}>
@@ -23,7 +27,7 @@ export default function SidePanel() {
           onClick={() => {
             const newMeal = {
               id: crypto.randomUUID(),
-              title: 'new meal',
+              createdAt: today,
               foods: [],
             };
             addMeal(newMeal);
