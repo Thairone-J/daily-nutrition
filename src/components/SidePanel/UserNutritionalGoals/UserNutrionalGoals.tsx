3fd84ef84345e2a35140e3026eb0e1854e0aa4da
Thairone-J <thairone.dev@gmail.com>
2025-06-01 17:25:57 -0300

    feat: add date select to side panel

--- src/components/SidePanel/UserNutritionalGoals/UserNutrionalGoals.tsx
+++ src/components/SidePanel/UserNutritionalGoals/UserNutrionalGoals.tsx
@@ -1,30 +1,38 @@
 import styles from './UserNutritionalGoals.module.scss';
 import ProgressBar from './ProgressBar';
-type HeaderProps = {
-  calorieGoal?: number;
-  consumedCalories?: number;
-};
+import { getTodayDateFormated } from '@/utils/dateUtils';
+import { useState } from 'react';
 
-export default function UserNutritionalGoals({ calorieGoal, consumedCalories }: HeaderProps) {
+export default function UserNutritionalGoals(calorieGoal: Number, consumedCalories: Number) {
+  const today = getTodayDateFormated();
+  const [selectedDate, setSelectedDate] = useState<string>(today);
   return (
     <div className={styles.wrapper}>
+      <div className={styles.dateSelect}>
+        <input
+          type="date"
+          defaultValue={today}
+          className={styles.dateInput}
+          onChange={(e) => {
+            setSelectedDate(e.target.value);
+            e.target.blur();
+          }}
+        />
+      </div>
       <div className={styles.header}>
         <img src="/pp.jpg" alt="" />
         <div className={styles.kcalGoal}>
           <h2>Metas nutricionais</h2>
           <p>
-            <span className={styles.consumedCalories}>{consumedCalories}</span> de {calorieGoal}{' '}
-            calorias 🔥
+            <span className={styles.consumedCalories}>{}</span> de {} calorias 🔥
           </p>
         </div>
-
-        
       </div>
       <div className={styles.goalsProgress}>
-          <ProgressBar label="Carboidratos" value={250} goal={250} />
-          <ProgressBar label="Proteínas" value={100} goal={250} />
-          <ProgressBar label="Gorduras" value={100} goal={250} />
-        </div>
+        <ProgressBar label="Carboidratos" value={250} goal={250} />
+        <ProgressBar label="Proteínas" value={100} goal={250} />
+        <ProgressBar label="Gorduras" value={100} goal={250} />
+      </div>
     </div>
   );
 }
